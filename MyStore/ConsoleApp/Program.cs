c56c7bda6947fc1779d0cc1fb2c02b5b9754bafc
Cutreson <dinhhongsontbpt@gmail.com>
2022-09-13 08:04:45 +0700

    clear all

---
+++ MyStore/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = @".\CUTRESON";
+            string database = "MyStore";
+            string username = "son";
+            string password = "son@123";
+            SQLConnector.SetConnectionInfo(server, database, username, password);
+            if(SQLConnector.TestConnection())
+            {
+                Console.WriteLine("Connect");
+            }
+            else
+            {
+                Console.WriteLine("Not connect");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyStore/DAL/SQLConnector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class SQLConnector
+    {
+        private static string myServerAddress, myDataBase, myUsername, myPassword;
+        private static string connectionString = string.Empty;
+        public static void SetConnectionInfo(string server, string database, string username, string password)
+        {
+            myServerAddress = server;
+            myDataBase = database;
+            myUsername = username;  
+            myPassword = password;
+            SetConnectionString();
+        }
+
+        private static void SetConnectionString()
+        {
+            string connectionTemplate = @"Server={0};Database={1};User Id={2};Password={3};";
+            connectionString = string.Format(connectionTemplate, myServerAddress, myDataBase, myUsername, myPassword);
+        }
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    connection.Close();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
